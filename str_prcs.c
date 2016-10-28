24c4ba3c59da62530cf4cb61fda109c43b586281
Dogafish <dogafish@seznam.cz>
2016-10-28 22:31:32 +0200

    small mistake

--- str_prcs.c
+++ str_prcs.c
@@ -13,7 +13,7 @@
 void* string_process(int id, char* str)
 {
     char *eptr;
-    if (id==TYPE_INT)
+    if (id == TYPE_INT)
         {
             int *number = malloc(sizeof(int));
             if (!number) return NULL;
@@ -23,7 +23,7 @@ void* string_process(int id, char* str)
                 *number = strtol(str,&eptr,10);
             return (void*)number;
         }
-    else if (id==TYPE_DOUBLE)
+    else if (id == TYPE_DOUBLE)
         {
             double *number = malloc(sizeof(double));
             if (!number) return NULL;
@@ -35,27 +35,27 @@ void* string_process(int id, char* str)
             int input=0, output=0, octal;
 //input a output sleduji retezec - input jej projede cely, jestlize narazi na specialni pripad, probehne prislusna akce, output dostane vysledek
 //octal slouzi jako pomocna promena pri prevodu cisla v osmickove soustave z retezce do desitkove soutavy
-            while (str[input]!='\0')
+            while (str[input] != '\0')
                 {
-                    if (str[input]=='\\')//vsechny escape sekvence zacinaji znakem '\'
+                    if (str[input] == '\\')//vsechny escape sekvence zacinaji znakem '\'
                         {
                             input++; //zjisitme, zda a ktera escape sekvence nastala
                             switch(str[input])
                                 {
                                     case '"':
-                                        str[output++]='\"';
+                                        str[output++] = '\"';
                                         input++;
                                         break;
                                     case 'n':
-                                        str[output++]='\n';
+                                        str[output++] = '\n';
                                         input++;
                                         break;
                                     case 't':
-                                        str[output++]='\t';
+                                        str[output++] = '\t';
                                         input++;
                                         break;
                                     case '\\':
-                                        str[output++]='\\';
+                                        str[output++] = '\\';
                                         input++;
                                         break;
                                     default:
@@ -64,31 +64,31 @@ void* string_process(int id, char* str)
                                             {
                                                 octal=(str[input]-'0')*64+(str[input+1]-'0')*8+(str[input+2]-'0');
                                                 */
-                                        if (str[input]>='0'&&str[input]<='3') //kontrola, zda je za znakem '\' trojmistne cislo v povolenem rozsahu provedena tak, aby nebyla prekrocena hranice stringu
-                                            if (str[input]>='0'&&str[input]<='7')
-                                                if (str[input]>='0'&&str[input]<='7')
-                                                    octal=(str[input]-'0')*64+(str[input+1]-'0')*8+(str[input+2]-'0');
+                                        if (str[input]>='0' && str[input]<='3') //kontrola, zda je za znakem '\' trojmistne cislo v povolenem rozsahu provedena tak, aby nebyla prekrocena hranice stringu
+                                            if (str[input+1]>='0' && str[input+1]<='7')
+                                                if (str[input+2]>='0' && str[input+2]<='7')
+                                                    octal = (str[input]-'0')*64 + (str[input+1]-'0')*8 + (str[input+2]-'0');
                                                 else
                                                     break;
                                             else
                                                 break;
                                         else
                                             break;
-                                        if (octal==0)
+                                        if (octal == 0)
                                             {
-                                                str[output++]='\\';
+                                                str[output++] = '\\';
                                                 break;
                                             }
                                         else
                                             {
-                                                str[output++]=octal;
-                                                input+=3;
+                                                str[output++] = octal;
+                                                input += 3;
                                                 break;
                                             }
                                 }
                         }
-                    str[output++]=str[input++];
+                    str[output++] = str[input++];
                 }
-            return(void*)str;
+            return (void*)str;
         }
 }
