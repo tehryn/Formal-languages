5fb05eae1ca20bd889fde3959aae71d3bf5b7c3e
Sava Nedeljkovic <savke7@live.com>
2016-11-21 00:18:31 +0100

    new big update

--- expression.h
+++ expression.h
@@ -1,8 +1,23 @@
 #ifndef expression_h_INCLUDED
 #define expression_h_INCLUDED
 
-#include <stdlib.h>
-#include "scanner.h"
+#define FATAL_ERROR(message, error_code) do { 	if(ma1[0]!=NULL) free(ma1[0]); if(ma1[1]!=NULL) free(ma1[1]); \
+												for(int i=0; i<ma2_top; i++) if(ma2[i]!=NULL) free(ma2[i]); \
+												fputs((message), stderr); \
+												return (error_code); } while(0)
+//
+
+#define ERR_LEXICAL_ANALYSIS 1
+#define ERR_SYNTACTIC_ANALYSIS 2
+#define ERR_SEM_NDEF_REDEF 3
+#define ERR_SEM_COMPATIBILITY 4
+#define ERR_SEM_OTHERS 6
+#define ERR_INPUT_NUMBER 7
+#define ERR_UNINICIALIZED_VAR 8
+#define ERR_DIVISION_ZERO 9
+#define ERR_OTHERS 10
+#define ERR_INTERN_FAULT 99
+
 
 struct stack_expresion
 {
@@ -11,17 +26,14 @@ struct stack_expresion
 	int top;
 };
 
-
-int expresion_in_to_post 	( token **postfix_token_array, token *return_token );
-int stack_expression_init 	( struct stack_expresion* s, int size  );
+int expr_analyze			( token t_in, token *t_out, token **postfix_token_array, int *token_count );
+int stack_expression_init 	( struct stack_expresion* s, int size );
 int stack_expression_destroy( struct stack_expresion* s );
 int stack_expression_empty 	( const struct stack_expresion* s );
 int stack_expression_full 	( const struct stack_expresion* s );
-int stack_expression_top 	( struct stack_expresion* s, token* t  );
+int stack_expression_top 	( struct stack_expresion* s, token* t );
 int stack_expression_pop 	( struct stack_expresion* s, token* t );
 int stack_expression_push	( struct stack_expresion* s, token t );
-int operator_priority 		(token t);
-void expression_fatal_error	(struct stack_expresion* s1, struct stack_expresion* s2, int err_num);
+int operator_priority 		( token t );
 
-
-#endif 	// expression_h_INCLUDED
+#endif	// expression_h_INCLUDED
