2174bdc7d486014aa68745a8b236aa5a07698662
tehryn <thetehryn@gmail.com>
2016-11-24 11:37:55 +0100

    Update test source file

--- test2.java
+++ test2.java
@@ -12,11 +12,36 @@
 	}
 	static int b;
 } */
+
+class Main {
+    static int input1;
+    static int input2;
+    static void run() {
+        ifj16.print("Nacteme 2x integer\n");
+        input1 = ifj16.readint();
+        input2 = ifj16.readint();
+        int x = proc_int.add(input1, input2);
+        String str = "%i + %i = %i\012";
+        ifj16.print(str, input1, input2, x);
+        str = "%i\t*\t%i\t=\t%i\n";
+        ifj16.print(str, input1, input2, proc_int.mul(input1, input2));
+        str = "Co je dobre?\n%i\t-\t%i\t=\t%i\nnebo\n%i\t-\t%i\t=\t%i\n";
+        ifj16.print(str, input1, input2, proc_int.sub(input1, input2), input1, input2, proc_int.sub(input1, input2));
+        str = "%i\t\\t%i\t=\t%i\n";
+        ifj16.print(str, input1, input2, proc_int.mul(input1, input2));
+        ifj16.print(str, input2, input1, proc_int.mul(input1, input2));
+        ifj16.print("A ted neco odporneho\n");
+        x = proc_int.add(input1,proc_int.mul(input1, proc_int.div(input1, proc_int.sub(input1, input2))));
+        ifj16.print("Vysledek odporne operace c. 1 je pry %i\n", x);
+        x = proc_int.foo();
+        ifj16.print("Vysledek odporne operace c. 2 je pry %i\n", x);
+        return;
+    }
+}
+
 class proc_int {
     static int add(int a, int b) {
         int c = a + b;
-        String str = "%i + %i = %i\012";
-        ifj16.print(str, a, b, c);
         calls = 1 + calls;
         return c;
     }
@@ -61,3 +86,4 @@ class proc_int {
     }
     static int sum = 0;
     static int calls = sum;
+}
