9c752e37a3e2a8fd02858647eb1eecbe6a9b0503
Sava Nedeljkovic <savke7@live.com>
2016-11-26 05:56:03 +0100

    vyhledavani v htab

--- expression.h
+++ expression.h
@@ -1,6 +1,16 @@
 #ifndef expression_h_INCLUDED
 #define expression_h_INCLUDED
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include "htab.h"
+#include "scanner.h"
+
+
+
 #define FATAL_ERROR(message, error_code) do { 	if(ma1[0]!=NULL) free(ma1[0]); if(ma1[1]!=NULL) free(ma1[1]); \
 												for(int i=0; i<ma2_top; i++) if(ma2[i]!=NULL) free(ma2[i]); \
 												fputs((message), stderr); \
@@ -26,7 +36,7 @@ struct stack_expresion
 	int top;
 };
 
-int expr_analyze			( token t_in, token *t_out, token **postfix_token_array, int *token_count );
+int expr_analyze 			( token t_in, token *t_out, token **postfix_token_array, int *token_count, int *expr_data_type, htab_t *global_table, htab_t *local_table, ...);
 int stack_expression_init 	( struct stack_expresion* s, int size );
 int stack_expression_destroy( struct stack_expresion* s );
 int stack_expression_empty 	( const struct stack_expresion* s );
@@ -34,6 +44,7 @@ int stack_expression_full 	( const struct stack_expresion* s );
 int stack_expression_top 	( struct stack_expresion* s, token* t );
 int stack_expression_pop 	( struct stack_expresion* s, token* t );
 int stack_expression_push	( struct stack_expresion* s, token t );
-int operator_priority 		( token t );
+int operator_priority 		(int op);
+int type_priority 			(int type);
 
 #endif	// expression_h_INCLUDED
