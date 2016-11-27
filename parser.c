fb3b78e17e8486dfab74571a7eba515e5cbbf96f
Dogafish <dogafish@seznam.cz>
2016-11-27 19:27:47 +0100

    update

--- parser.c
+++ parser.c
@@ -718,6 +718,7 @@ int analysis (stack_int_t *s, unsigned runtime, stack_htab Stack_of_TableSymbols
 						}
 
 						TableItem->data_type = type;
+						TableItem->func_or_var = 1;
 					}
 
 					if (stack_int_push(s, 1, P_DEF_ARGUMENTS2) < 0)
