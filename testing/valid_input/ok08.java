76fd29c0945dd704da960090c3422026a2aba82f
Sava Nedeljkovic <savke7@live.com>
2016-12-01 08:24:53 +0100

    i -> int i

--- testing/valid_input/ok08.java
+++ testing/valid_input/ok08.java
@@ -1,6 +1,6 @@
 class Main {
     static int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = 0000_0012;
     static void run() {
-        i = 0xF;
+        int i = 0xF;
     }
 }
