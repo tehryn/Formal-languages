4f1562521cb3d634e6d2b774cef145ebda73b7a2
tehryn <thetehryn@gmail.com>
2016-12-03 15:40:08 +0100

    new tests

---
+++ tests/diff_check/diff10.java
@@ -0,0 +1,11 @@
+class Main {
+    static void run(){
+        int a = 5;
+        int b = 3;
+        if (a > b) {
+            ifj16.print(a);
+            ifj16.print("\n");
+        }
+        ifj16.print(b);
+    }
+}
---
+++ tests/diff_check/diff17.java
@@ -0,0 +1,10 @@
+class Main {
+    static void run(){
+        double a = 5.6;
+        int i = 1;
+        if (a < i)
+            if16.print("Wrong\n");
+        else
+            ifj16.print(a);
+    }
+}
