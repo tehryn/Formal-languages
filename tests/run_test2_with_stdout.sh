c7a62af5c1b1fa8b2eac5f51ecc80ffcd22edaf9
Sava Nedeljkovic <savke7@live.com>
2016-12-04 12:31:23 +0100

    updated t new run_test

--- tests/run_test2_with_stdout.sh
+++ tests/run_test2_with_stdout.sh
@@ -1,87 +1,96 @@
-declare -a valide_tests=("ok1.java" "ok2.java" "ok3.java" "ok4.java" "ok5.java" "ok6.java" "ok7.java" "ok8.java" "ok9.java" "ok10.java" "ok11.java" "ok12.java" "ok13.java" "ok14.java" "ok15.java")
-declare -a invalide_syntax=("err2.java" "err3.java" "err4.java" "err5.java" "err10.java" "err26.java" "err28.java" "err29.java" "err38.java" "err39.java" "err40.java")
-declare -a invalide_lexical=("err6.java" "err7.java")
-declare -a invalide_semantic_def=("err1.java" "err8.java" "err9.java" "err11.java" "err13.java" "err14.java" "err15.java" "err18.java" "err20.java" "err31.java" "err36.java")
-declare -a invalide_semantic_compat=("err12.java" "err16.java" "err17.java" "err19.java" "err21.java" "err22.java" "err23.java" "err24.java" "err25.java" "err27.java" "err30.java" "err32.java" "err33.java" "err34.java" "err35.java" "err37.java")
-declare -a invalide_semantic_other=()
-declare -a invalide_run_no_init=("err41.java" "err42.java" "err43.java" "err44.java" "err45.java")
-#declare -a newest=("ok1.java" "ok2.java" "err40.java" "err20.java" "err16.java" "err17.java" "err21.java" "err22.java" "err23.java" "err30.java" "err33.java" "err37.java")
+#!/bin/bash
+if [ $# -eq 0 ]
+then
+	GREEN='\033[1;32m'
+	RED='\033[1;31m'
+	NC='\033[0m' # No Color
+fi
 
-
-
-for file in ${valide_tests[@]}
+for file in ./valid_input/*.java
 do
 	echo -e "\n"; ./IFJ16 $file >/dev/null
 	ret=$?
 	if [[ $ret == 0 ]]; then
-		echo "$file: SUCCES - interpret returned with $ret"
+		echo -e "${GREEN}$file: SUCCES - interpret returned with $ret"
 	else
-		echo "$file: ERROR - interpret returned with $ret but should have returned with 0"
+		echo -e "${RED}$file: ERROR - interpret returned with $ret but should have returned with 0"
+		echo -e "${NC}------------------------------------------------------------------------------------"
+		cat tmp.txt
+		echo "===================================================================================="
 	fi
 done
 
-for file in ${invalide_lexical[@]}
+for file in ./invalid_lex/*.java
 do
 	echo -e "\n"; ./IFJ16 $file >/dev/null
 	ret=$?
 	if [[ $ret == 1 ]]; then
-		echo "$file: SUCCES - interpret returned with $ret"
+		echo -e "${GREEN}$file: SUCCES - interpret returned with $ret"
 	else
-		echo "$file: ERROR - interpret returned with $ret but should have returned with 1"
+		echo -e "${RED}$file: ERROR - interpret returned with $ret but should have returned with 1"
+		echo -e "${NC}------------------------------------------------------------------------------------"
+		cat tmp.txt
+		echo "===================================================================================="
 	fi
 done
 
-for file in ${invalide_syntax[@]}
+for file in ./invalid_syn/*.java
 do
 	echo -e "\n"; ./IFJ16 $file >/dev/null
 	ret=$?
 	if [[ $ret == 2 ]]; then
-		echo "$file: SUCCES - interpret returned with $ret"
+		echo -e "${GREEN}$file: SUCCES - interpret returned with $ret"
 	else
-		echo "$file: ERROR - interpret returned with $ret but should have returned with 2"
+		echo -e "${RED}$file: ERROR - interpret returned with $ret but should have returned with 2"
+		echo -e "${NC}------------------------------------------------------------------------------------"
+		cat tmp.txt
+		echo "===================================================================================="
 	fi
 done
 
-for file in ${invalide_semantic_def[@]}
+for file in ./invalid_sem/def/*.java
 do
 	echo -e "\n"; ./IFJ16 $file >/dev/null
 	ret=$?
 	if [[ $ret == 3 ]]; then
-		echo "$file: SUCCES - interpret returned with $ret"
+		echo -e "${GREEN}$file: SUCCES - interpret returned with $ret"
 	else
-		echo "$file: ERROR - interpret returned with $ret but should have returned with 3"
+		echo -e "${RED}$file: ERROR - interpret returned with $ret but should have returned with 3"
+		echo -e "${NC}------------------------------------------------------------------------------------"
+		cat tmp.txt
+		echo "===================================================================================="
 	fi
 done
 
-for file in ${invalide_semantic_compat[@]}
+for file in ./invalid_sem/com/*.java
 do
 	echo -e "\n"; ./IFJ16 $file >/dev/null
 	ret=$?
 	if [[ $ret == 4 ]]; then
-		echo "$file: SUCCES - interpret returned with $ret"
+		echo -e "${GREEN}$file: SUCCES - interpret returned with $ret"
 	else
-		echo "$file: ERROR - interpret returned with $ret but should have returned with 4"
+		echo -e "${RED}$file: ERROR - interpret returned with $ret but should have returned with 4"
+		echo -e "${NC}------------------------------------------------------------------------------------"
+		cat tmp.txt
+		echo "===================================================================================="
 	fi
 done
 
-for file in ${invalide_semantic_other[@]}
+for file in ./invalid_run/ini/*.java
 do
 	echo -e "\n"; ./IFJ16 $file >/dev/null
 	ret=$?
-	if [[ $ret == 6 ]]; then
-		echo "$file: SUCCES - interpret returned with $ret"
+	if [[ $ret == 8 ]]; then
+		echo -e "${GREEN}$file: SUCCES - interpret returned with $ret"
 	else
-		echo "$file: ERROR - interpret returned with $ret but should have returned with 6"
+		echo -e "${RED}$file: ERROR - interpret returned with $ret but should have returned with 8"
+		echo -e "${NC}------------------------------------------------------------------------------------"
+		cat tmp.txt
+		echo "===================================================================================="
 	fi
 done
 
-for file in ${invalide_run_no_init[@]}
-do
-	./IFJ16 $file >/dev/null 2>&1
-	ret=$?
-	if [[ $ret == 8 ]]; then
-		echo "$file: SUCCES - interpret returned with $ret"
-	else
-		echo "$file: ERROR - interpret returned with $ret but should have returned with 8"
-	fi
-done
+
+
+
+rm tmp.txt
