c35a107c0006ca20cf218388d43aed94bc518305
Dogafish <dogafish@seznam.cz>
2016-12-05 09:58:51 +0100

    update

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@
 CFLAGS = -std=c99 -pedantic -Wall -Wextra -g
 default: IFJ16
 
-IFJ16: main.o error.o scanner.o garbage_collector.o parser.o ial.o structures.o expression.o
+IFJ16: main.o interpret.o error.o scanner.o garbage_collector.o parser.o ial.o structures.o expression.o
 	gcc $(CFLAGS) -o IFJ16 $^
 
 %.o: %.c %.h
--- interpret.h
+++ interpret.h
@@ -9,7 +9,6 @@
 #include "scanner.h"
 #include "structures.h"
 #include "expression.h"
-#include "tmp.c"
 
 enum {
 	I_ASSIGMENT = 68+1,	//69
@@ -46,5 +45,37 @@ int inter(Instr_List *L, stack_htab *I_Htable);
 char *IntToString(int x);
 char *DoubleToString(double x);
 char *Conc_Str(char *s1, char *s2);
+
+/**
+ * Search for item of local or global hash table in the stack
+ * @param  stack	stack where item will be searched
+ * @param  key		name of variable or function which will be searched
+ * @return			pointer to item of hash. table where searched thing is, or NULL if the search was not successful
+ * @pre				stack has been inicializated
+ */
+htab_item * stack_htab_find_htab_item(stack_htab * stack, char * key)
+{
+	htab_t * Table = NULL;
+	htab_item * TableItem = NULL;
+	if (is_full_ident(key, strlen(key)))
+	{
+		Table = stack_htab_get_first(stack);
+		if (Table == NULL)
+			return NULL;
+		TableItem = htab_find_item(Table, key);
+		return TableItem; // could be NULL
+	}
+	else if (is_simple_ident(key, strlen(key)))
+	{
+		Table = stack_htab_get_item(stack, 0);
+		if (Table == NULL)
+			return NULL;
+		TableItem = htab_find_item(Table, key);
+		return TableItem; // could be NULL
+	}
+	return NULL;
+}
+
+
 
 #endif
