46511c526c55e0730e4ef54c81f7623c1ba34e72
tehryn <thetehryn@gmail.com>
2016-12-08 12:34:00 +0100

    update

--- tests/diff_check/diff23.java
+++ tests/diff_check/diff23.java
@@ -2,7 +2,7 @@ class Main {
     static void run(){
         int i = 10;
         while (i > 0) {
-            if (i/2 < (i/2)*2)
+            if ((i/2)*2 < i )// 4 < 
                 ifj16.print(i + "\n");
             i = i - 1;
         }
