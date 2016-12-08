a8a477346be9820710952034ca5db454a8e2bf9f
tehryn <thetehryn@gmail.com>
2016-12-08 11:42:27 +0100

    update

--- tests/diff_check/diff11.java
+++ tests/diff_check/diff11.java
@@ -3,8 +3,8 @@ class Main {
         int a = 5;
         int b = a + 1;
         if ((a+1) == b) {
-            ifj16.print(a + b);
+            ifj16.print(a + "" +b);
         }
-        ifj16.print(b+a);
+        ifj16.print(b + a);
     }
 }
