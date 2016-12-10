1fa80bb13a044ad69d006f050e7a9ced37d55332
tehryn <thetehryn@gmail.com>
2016-12-10 16:48:35 +0100

    update

--- tests/diff_check/diff47.java
+++ tests/diff_check/diff47.java
@@ -1,5 +1,10 @@
 class Main {
     static void run() {
-
+        boolean a = !(5 < 8);
+        if (!! !! !a) {
+            ifj16.print("true");
+            return;
+        }
+        ifj16.print("false");
     }
 }
