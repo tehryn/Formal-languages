e8517925f8af00eeff30f4adced97c5094875312
Sava Nedeljkovic <savke7@live.com>
2016-12-11 19:40:29 +0100

    doxygen coments, Sava

--- ial.h
+++ ial.h
@@ -1,10 +1,8 @@
 /**
-* Author: TODO
-* Login: TODO
+* Authors: Miroslava Misova, Nemanja Vasiljevic, Jiri Matejka, Sava Nedeljkovic
 * School: VUT FIT, BRNO
 * Project: Interpret for IFJ16
 * gcc version: 5.4.0 (ubuntu 16.04.2)
-* Date: TODO
 **/
 
 #ifndef IAL
@@ -25,25 +23,34 @@
 #define AlphabetArray 256
 
 /**
- * [length description]
- * @param  string [description]
- * @return        [description]
+ * Function returns length of 0 terminated string.
+ * @param  string array of chars
+ * @pre string!=NULL
+ * @return integer value, length of string.
  */
 int length(char * string);
 
 /**
- * [substring description]
- * @param  s [description]
- * @param  i [description]
- * @param  n [description]
- * @return   [description]
+ * Returns substring of a given length, beginning at a given position of a given string
+ * Function allocates new memory for a substring
+ * @param  s input string 
+ * @param  i index where the substring begins
+ * @param  n length of a substring
+ * @pre s != NULL
+ * @pre n > 0
+ * @pre i => 0
+ * @pre strlen(s) >= n+i
+ * @return array of chars containing found substring
  */
 char * substring(char * s, int i, int n);
 
 /**
- * [shellsort description]
- * @param  str [description]
- * @return     [description]
+ * Returns sorted array of a chars.
+ * Function uses Shell sort algorithm.
+ * Function allocates new memory for a return array of chars.
+ * @param  str input string 
+ * @pre s != NULL
+ * @return sorted array of a chars.
  */
 char * shellsort(char * str);
 
@@ -62,76 +69,80 @@ int find( char *s,  char *search);
  */
 void computeJumps( char *string, int badchar[AlphabetArray]);
 
-/**
- *
+/**  \struct t_stack_int
+ * \brief Structure for stack of integers
  */
 typedef struct t_stack_int
 {
-	/** */
+	/** Index of a top element in a stack */
 	int top;
-	/** */
+	/** Size of stack (array length) */
 	int size;
-	/** */
+	/** Pointer to an array of integers */
 	int *data; // array
 } stack_int_t;
 
-/**
- * [stack_int_create  description]
- * @param  stack [description]
- * @param  n     [description]
- * @return       [description]
+/** 
+ * \brief Function initializes a stack, allocates required memory and sets its variables.
+ * \param[in, out] stack pointer to a stack.
+ * \param[in] n max. number of elements which stack could contain.
+ * \pre stack!=NULL
+ * \pre n!=0
+ * \post stack!=NULL
+ * \return integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
  */
 int stack_int_create ( struct t_stack_int * stack, int n );
 
-/**
- * [stack_int_destroy description]
- * @param stack [description]
+/** 
+ * \brief Function destroys a stack, frees its memory and sets its variables.
+ * \param[in, out] s pointer to a stack.
+ * \pre stack!=NULL
  */
 void stack_int_destroy( struct t_stack_int * stack );
 
 /**
- * [stack_int_push description]
- * @param  stack   [description]
- * @param  num     [description]
- * @param  VARARGS [description]
- * @return         [description]
+ * Function pushes given elements to the stack.
+ * @param  stack   pointer to a stack.
+ * @param  num     number of elements which should be pushed to the stack
+ * @param  VARARGS integer values which should be oushed to the stack
+ * @return         integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
  */
 int stack_int_push(struct t_stack_int* stack, int num, ...);
 
 /**
- * [stack_int_pop description]
- * @param  stack [description]
- * @return       [description]
+ * Function pops and gives back top element from the stack.
+ * @param  stack pointer to a stack.
+ * @return integer value, top element from the stack.
  */
 int stack_int_pop(struct t_stack_int* stack);
 
 /**
- * [stack_int_top description]
- * @param  stack [description]
- * @param  var   [description]
- * @return       [description]
+ * Function gives back top element from the stack.
+ * @param  stack pointer to a stack.
+ * @param  var   top element from the stack.
+ * @return       integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
  */
 int stack_int_top(struct t_stack_int* stack, int* var);
 
 /**
- * [stack_int_clean description]
- * @param  stack [description]
- * @param  n     [description]
- * @return       [description]
+ * Decrements stack pointer by given number of elements.
+ * @param  stack pointer to a stack.
+ * @param  n     number of elements which should be removed from stack. 
+ * @return       integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
  */
 int stack_int_clean(struct t_stack_int* stack, int n);
 
 /**
- * [stack_int_is_empty description]
- * @param  stack [description]
- * @return       [description]
+ * Function checks whether stack is empty.
+ * @param  stack pointer to a stack.
+ * @return integer value which tells, if the stack is empty, 0 -> not empty, !=0 -> empty.
  */
 int stack_int_is_empty( struct t_stack_int * stack );
 
 /**
- * [stack_int_is_full description]
- * @param  stack [description]
- * @return       [description]
+ * Function checks whether stack is full.
+ * @param  stack pointer to a stack.
+ * @return integer value which tells, if the stack is full, 0 -> not full, !=0 -> full.
  */
 int stack_int_is_full( struct t_stack_int * stack );
 
