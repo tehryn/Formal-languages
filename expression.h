0577f76cf35a0809096e653883545b704147860a
Sava Nedeljkovic <savke7@live.com>
2016-12-11 14:25:34 +0100

    doxygen comments

--- expression.h
+++ expression.h
@@ -1,3 +1,15 @@
+/**
+ * \file   expression.h
+ * \author Sava Nedeljkovic, xnedel08
+ * \date   14.12.2015
+ * \brief  Documentation for expression processing
+ * \details This module is used for processing expressions.
+ *  It checks whether expressions follows allowed rules.
+ *  Final expression is converted to postfix format.
+ */
+
+
+
 #ifndef expression_h_INCLUDED
 #define expression_h_INCLUDED
 
@@ -10,6 +22,15 @@
 #include "scanner.h"
 
 
+/** @defgroup expression_processing Expression processing */
+/** @addtogroup expression_processing
+  * @brief Precedence analysis and expression processing
+  * of the language IFJ16.
+  * @{
+  */
+
+  
+/*! Error constants */
 #define ERR_WARNING 0
 #define ERR_LEXICAL_ANALYSIS 1
 #define ERR_SYNTACTIC_ANALYSIS 2
@@ -37,40 +58,183 @@
 							} 																							\
 						} while(0)
 
-
+/**  \struct stack_expresion
+ * \brief Structure for stack of tokens
+ */
 struct stack_expresion
 {
+	/** \brief Pointer to an array of tokens */
 	token *arr;
+	/** \brief Size of stack (array length) */
 	int size;
+	/** \brief Index of a top element in a stack */
 	int top;
 };
 
-/*
-// t_in -> vstupni token
-// t_out -> ukazatel na token, kde bude ulozena hodnota posledniho tokenu
-// postfix_token_array -> ukazatel na pole tokenu, kde budou ulozeny jednotlive tokeny vyrazu ve formatu postfix
-// token_count -> ukazatel na int, kde bude ulozen pocet tokenu ve vyslednem poli tokenu
-// expr_data_type -> ukazatel na int, kde bude ulozena hodnota datoveho typu celeho vyrazu
-// global_table a local_table -> hash. tabulky
-// priklad volani:
-//	token t_in = get_token();
-//	token t_out;
-//	token *postfix_token_array;
-//	int token_count, expr_data_type;
-//	int expr_analyze(t_in, &t_out, &postfix_token_array, &token_count, &expr_data_type, global_table, local_table);
-*/
-int expr_analyze 		( token t_in, token *t_out, char* class_name, int error_6_flag, token **postfix_token_array, int *token_count, int *expr_data_type, htab_t *global_table, htab_t *local_table, ...);
-int stack_expression_init 	( struct stack_expresion* s, int size );
-int stack_expression_destroy	( struct stack_expresion* s );
-int stack_expression_empty 	( const struct stack_expresion* s );
-int stack_expression_full 	( const struct stack_expresion* s );
-int stack_expression_top 	( struct stack_expresion* s, token* t );
-int stack_expression_pop 	( struct stack_expresion* s, token* t );
-int stack_expression_push	( struct stack_expresion* s, token t );
-int operator_priority 		(int op);
-int type_priority 		(int type);
-int type_name_convertion 	(int type);
-void print_token		(token t, int id_flag);
-void print_token_array		(token * arr, int id_flag);
 
+/** 
+ * \brief Function analyzes precedence and converts expression to postfix format.
+ * \param[in] t_in first token in an expression.
+ * \param[out] t_out last token red by the function, which is not contained in a final expression.
+ * \param[in] class_name name of a class, which contains processed expression.
+ * \param[in] error_6_flag flag which determines whether error number 6 could be returned.
+ * \param[out] postfix_token_array final postfix expression.
+ * \param[out] token_count number of tokens in a retuned expression array.
+ * \param[out] expr_data_type data type of the retuned expression.
+ * \param[in] global_table global table of symbols.
+ * \param[in] local_table local table of symbols.
+ * \pre class_name!=NULL
+ * \pre global_table!=NULL
+ * \pre local_table!=NULL
+ * \pre error_6_flag==1 || error_6_flag==0
+ * \post postfix_token_array!=NULL
+ * \post t_out!=NULL
+ * \post token_count!=0
+ * \post postfix_token_array[token_count-1]==END_EXPR
+ * \return integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
+ */
+int expr_analyze 				( token t_in, token *t_out, char* class_name, int error_6_flag, token **postfix_token_array, int *token_count, int *expr_data_type, htab_t *global_table, htab_t *local_table, ...);
+
+
+
+/** 
+ * \brief Function initializes a stack, allocates required memory and sets its variables.
+ * \param[in, out] s pointer to a stack.
+ * \param[in] size max. number of tokens which stack could contain.
+ * \pre s!=NULL
+ * \pre size!=0
+ * \post s!=NULL
+ * \return integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
+ */
+int stack_expression_init 		( struct stack_expresion* s, int size );
+
+
+
+/** 
+ * \brief Function destroys a stack, frees its memory and sets its variables.
+ * \param[in, out] s pointer to a stack.
+ * \pre s!=NULL
+ * \return integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
+ */
+int stack_expression_destroy	( struct stack_expresion* s );
+
+
+
+/** 
+ * \brief Function checks whether stack is empty.
+ * \param[in] s pointer to a stack.
+ * \pre s!=NULL
+ * \return integer value which tells, if the stack is empty, 0 -> not empty, !=0 -> empty.
+ */
+int stack_expression_empty 		( const struct stack_expresion* s );
+
+
+
+/** 
+ * \brief Function checks whether stack is empty.
+ * \param[in] s pointer to a stack.
+ * \pre s!=NULL
+ * \return integer value which tells, if the stack is full, 0 -> not full, !=0 -> full.
+ */
+int stack_expression_full 		( const struct stack_expresion* s );
+
+
+
+/** 
+ * \brief Function gives back top element from the stack.
+ * \param[in] s pointer to a stack.
+ * \param[out] t top element from the stack.
+ * \pre s!=NULL
+ * \pre t!=NULL
+ * \pre s is not empty
+ * \post t!=NULL
+ * \return integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
+ */
+int stack_expression_top 		( struct stack_expresion* s, token* t );
+
+
+
+/** 
+ * \brief Function pops and gives back top element from the stack.
+ * \param[in] s pointer to a stack.
+ * \param[out] t top element from the stack.
+ * \pre s!=NULL
+ * \pre t!=NULL
+ * \pre s is not empty
+ * \post t!=NULL
+ * \post s->size == s->size-1
+ * \return integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
+ */
+int stack_expression_pop 		( struct stack_expresion* s, token* t );
+
+
+
+/** 
+ * \brief Function pushes given element to the stack.
+ * \param[in] s pointer to a stack.
+ * \param[out] t top which should be pushed to the stack.
+ * \pre s!=NULL
+ * \pre s is not full
+ * \post s->size == s->size+1
+ * \post s->arr[s->size] == t
+ * \return integer value which tells, how the whole processed has been executed, 0 -> no error, !=0 -> error.
+ */
+int stack_expression_push		( struct stack_expresion* s, token t );
+
+
+
+/** 
+ * \brief Function tells the priority of a given operator.
+ * \param[in] op operator symbol.
+ * \pre op>=0
+ * \return integer value which tells the priority of a given operator.
+ */
+int operator_priority 			(int op);
+
+
+
+/** 
+ * \brief Function tells the priority of a given data type,
+ *  which is later used for determining data type of the whole expression.
+ * \param[in] type data type symbol.
+ * \pre type>=0
+ * \return integer value which tells the priority of a given data type.
+ */
+int type_priority 				(int type);
+
+
+
+/** 
+ * \brief Function converts names of the given data type,
+ *  so it could be understood by the function expr_analyze().
+ * \param[in] type data type symbol.
+ * \return converted integer value which tells the what is the given data type.
+ */
+int type_name_convertion 		(int type);
+
+
+
+/** 
+ * \brief Function prints token value (and its id) to STDERR.
+ *  This function is only used for debugging.
+ * \param[in] t token which should be printed.
+ * \param[in] id_flag value which determines whether the id of a token should be printed as well.
+ * \pre id_flag == 0 || id_flag == 1
+ */
+void print_token				(token t, int id_flag);
+
+
+
+/** 
+ * \brief Function prints array of tokens (and their ids) to STDERR.
+ *  This function is only used for debugging.
+ * \param[in] arr array of tokens which should be printed.
+ * \param[in] id_flag value which determines whether the id of a tokens should be printed as well.
+ * \pre id_flag == 0 || id_flag == 1
+ */
+void print_token_array			(token * arr, int id_flag);
+
+
+
+/** @} */
 #endif	// expression_h_INCLUDED
